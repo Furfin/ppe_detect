123f5e7f972cc07f0a8dd31cea80774a29abd566
Furfin <furf1n228@gmail.com>
2022-05-24 21:14:32 +0300

    pravki!

--- app.py
+++ app.py
@@ -45,31 +45,6 @@ def gen_data(thread_id):
             yield data + "<br>"
     yield f"<a href = /video/{thread_id}>Video</a>"
 
-@app.route('/data/<int:thread_id>')
-def progress(thread_id):
-    data = ''
-    for track in Track.query.filter_by(thread_id = thread_id):
-        data += str(track.data) + '</br>'
-    return data+'!'
-
-@app.route('/feed/<int:thread_id>')
-def feed(thread_id):
-        return Response(gen_frames(thread_id),mimetype='multipart/x-mixed-replace; boundary=frame')
-
-@app.route('/<int:thread_id>')
-def output(thread_id):
-    return render_template('index.html',thread = thread_id)
-
-def gen_frames(thread_id):
-    while True:
-        frame = exporting_threads[thread_id].img
-        if frame == 0:
-            break
-        else:
-            yield b'--frame\r\nContent-Type: image/jpeg\r\n\r\n'
-            yield frame
-            yield b'\r\n\r\n'
-            
 @app.route('/')
 def index():
    return render_template('upload.html')
